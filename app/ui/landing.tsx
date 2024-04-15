8085b97c00170c883a2c48343efc196ca470262b
Vader <manavhirani@gmail.com>
2024-04-15 03:42:05 -0400

    Fixed a bug where with caused horizontal scroll to appear

--- app/ui/landing.tsx
+++ app/ui/landing.tsx
@@ -11,13 +11,13 @@ const jetbrainsmono = JetBrains_Mono({
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col w-screen gap-4 p-4 divide-y divide-white/10">
+    <div className="flex flex-col gap-4 p-4 divide-y divide-white/10">
       {/* <div className="w-full sm:h-16 md:h-32"></div> */}
       <div className="flex">
         <div className="flex flex-col items-center w-full gap-8 p-8 lg:flex-row">
           <div className="flex items-center justify-center w-full p-4 text-center lg:text-right lg:min-h-64 lg:justify-end">
             <div className="space-y-4">
-              <h1 className="text-6xl font-bold text-transparent sm:text-8xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text font-jetbrains_mono">
+              <h1 className="text-6xl font-bold text-transparent sm:text-8xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text font-jetbrains_mono text-balance">
                 {"full stack dev"}
               </h1>
             </div>
