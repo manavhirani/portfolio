bb6856d44b0d6c9e0a5b4735c357a3f15d3e9428
Vader <manavhirani@gmail.com>
2024-11-13 22:24:51 -0500

    Feature: Heading, Socials, Bio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
 export default function Home() {
   return (
-    <>
-      <h1 className="lowercase p-12">manavhirani</h1>
-    </>
+    <div className="p-12">
+      <h1 className="">Manav Hirani</h1>
+      <p>AI Engineer & Researcher</p>
+      <ul>
+        <li>
+          <a href="https://github.com/manavhirani">GitHub</a>
+        </li>
+        <li>
+          <a href="https://github.com/manavhirani">𝕏</a>
+        </li>
+        <li>
+          <a href="https://scholar.google.com/citations?hl=en&user=eIF9sJIAAAAJ&view_op=list_works&gmla=AL3_zihHc7tdP22APjSCfz0o7J7ospE4DbLrq1mjrxvnbT-4Zr27cE-slSvUta6JBM30Z78R6htPEQ_09ZTs3mq61gPCp7hmponZ1w">scholar</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/manav-hirani/">LinkedIn</a>
+        </li>
+      </ul>
+    </div>
     );
 }
