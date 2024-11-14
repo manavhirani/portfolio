c06872bbe9d8dbbe35713f6a2e0a1d4b9489ceaf
Vader <manavhirani@gmail.com>
2024-11-14 00:50:57 -0500

    Fix: Updated README.md

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <div className="p-12">
-      <h1 className="">Manav Hirani</h1>
+      <h1 className="text-3xl">Manav Hirani</h1>
       <p>AI Engineer & Researcher</p>
+      <p>Scaling LLM's at Syracuse University</p>
       <ul>
         <li>
           <a href="https://github.com/manavhirani">GitHub</a>
@@ -11,12 +12,13 @@ export default function Home() {
           <a href="https://github.com/manavhirani">𝕏</a>
         </li>
         <li>
-          <a href="https://scholar.google.com/citations?hl=en&user=eIF9sJIAAAAJ&view_op=list_works&gmla=AL3_zihHc7tdP22APjSCfz0o7J7ospE4DbLrq1mjrxvnbT-4Zr27cE-slSvUta6JBM30Z78R6htPEQ_09ZTs3mq61gPCp7hmponZ1w">scholar</a>
+          <a href="https://scholar.google.com/citations?hl=en&user=eIF9sJIAAAAJ&view_op=list_works&gmla=AL3_zihHc7tdP22APjSCfz0o7J7ospE4DbLrq1mjrxvnbT-4Zr27cE-slSvUta6JBM30Z78R6htPEQ_09ZTs3mq61gPCp7hmponZ1w">Scholar</a>
         </li>
         <li>
           <a href="https://www.linkedin.com/in/manav-hirani/">LinkedIn</a>
         </li>
       </ul>
+      <p>Email: <a href="mailto:mxhirani@gmail.com">mxhirani@gmail.com</a></p>
     </div>
     );
 }
