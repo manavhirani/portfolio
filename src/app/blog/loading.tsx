bb0f963053757f116a57ad1c63a83e86e1ff4da6
Vader <manavhirani@gmail.com>
2024-11-18 14:26:49 -0500

    Feature: Added grid view to skeleton

--- src/app/blog/loading.tsx
+++ src/app/blog/loading.tsx
@@ -1,9 +1,14 @@
 import { SkeletonCard } from "@/components/ui/skeleton-card"
 
 export default function BlogLoading() {
-    return (
-        <div className="">
-            <SkeletonCard />
-        </div>
-    )
+	return (
+		<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-">
+			{ Array.from({ length: 9 }).map((_, i) =>  (
+				<div key={ i }>
+						<SkeletonCard />
+					</div>
+				))
+			}
+		</div>
+	)	
 }
