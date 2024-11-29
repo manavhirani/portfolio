7b35ac60b7b87a9ca2228661f0c712d3dac74a2f
Manav Hirani <manavhirani@gmail.com>
2024-11-29 01:37:45 -0500

    Fixed dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/markdown/home/hero.mdx";
 export default function Home() {
   return (
     <div className="p-6">
-      <div className="">
+      <div className="prose dark:prose-invert dark:prose-purple">
         <Hero />
       </div>
     </div>
